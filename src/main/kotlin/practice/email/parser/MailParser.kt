c330423ae03bb36f9c0f3f6010defd77f0426ac3
Zhuk Pavel <ppzhuk@users.noreply.github.com>
2016-03-15 03:19:46 +0300

    start library implementation (task 4)

--- src/main/kotlin/practice/email/parser/MailParser.kt
+++ src/main/kotlin/practice/email/parser/MailParser.kt
@@ -1,5 +1,19 @@
 package practice.email.parser
 
-class MailParser {
+import java.util.*
 
+object ParseMode{
+    val SIMPLE = 0
+    val QUOTE  = 1
+    val QUOTE_DEEP = 2
+    val QUOTE_SMART = 3
+    val QUOTE_SMART_DEEP = 4
+}
+
+data class Email(val from: String, val to: String, val date: Date, val subject: String, val content: EmailContent)
+data class EmailContent(val doby: String)
+
+fun parseEml(path: String, mode: Int = ParseMode.SIMPLE, deep: Int = 1): Email? {
+    // not implemented yet
+    return null
 }
