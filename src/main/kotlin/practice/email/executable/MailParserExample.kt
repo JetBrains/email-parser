2c0a7bec14bb0ffa54361a6446e06cca3580a7da
Zhuk Pavel <ppzhuk@users.noreply.github.com>
2016-04-01 02:03:02 +0300

    configure part of testcases, modify executable output

--- src/main/kotlin/practice/email/executable/MailParserExample.kt
+++ src/main/kotlin/practice/email/executable/MailParserExample.kt
@@ -1,18 +1,10 @@
 package practice.email.executable
 
+import practice.email.parser.Content
 import practice.email.parser.EmailContentParseMode
 import practice.email.parser.parseEml
 import practice.email.parser.parseEmlWithContent
 import java.io.File
-import java.io.FileInputStream
-import java.io.InputStream
-import java.util.Properties
-
-import javax.mail.Message
-import javax.mail.Session
-import javax.mail.internet.InternetAddress
-import javax.mail.internet.MimeMessage
-import javax.mail.internet.MimeMultipart
 
 private val incorrectFileMsg = "Given file is not exists."
 private val noFilePathMsg = "There isn't any path to target file. Try again and input path as a first command-line argument."
@@ -22,83 +14,40 @@ fun main(args: Array<String>) {
         val file = File(args[0])
         if (file.exists()) {
             var email = parseEml(file)
-            println("Just an email:")
+            println("********** Just an email **********")
             println(email)
             
             email = parseEmlWithContent(file, EmailContentParseMode.ONE)
-            println("Email with parsed content (mode ONE):")
-            println(email)
-            
-            println("Top level content's components:")
-            println("body:")
+            println("********** Content's components (mode ONE) **********")
+            println("*** body ***")
             println(email.content.body)
-            println("quote:")
+            println("*** quote ***")
             println(email.content.quote)
-            println("signature:")
+            println("*** signature ***")
             println(email.content.signature)
-
+            println()
             
             email = parseEmlWithContent(file, EmailContentParseMode.DEEP)
-            println("Email with parsed content (mode DEEP):")
-            println(email)
-
-            println("Top level content's components:")
-            println("body:")
-            println(email.content.body)
-            println("quote:")
-            println(email.content.quote)
-            println("signature:")
-            println(email.content.signature)
-
+            println("********** Content's components (mode DEEP) **********")
+            
+            var currentContent: Content? = email.content
+            var i = 0
+            do {
+                println("*** body$i ***")
+                println(currentContent?.body)
+                println("*** quote$i ***")
+                println(currentContent?.quote)
+                println("*** signature$i ***")
+                println(currentContent?.signature)
+                println()
+                
+                currentContent = currentContent?.quote
+                ++i
+            } while (currentContent != null)
         } else {
             println(incorrectFileMsg)
         }
     } else {
         println(noFilePathMsg)
     }
-
-
-}
-
-// just some func for testing
-fun helperFun() {
-    val props: Properties = System.getProperties()
-    val mailSession: Session = Session.getDefaultInstance(props)
-    val source: InputStream = FileInputStream(File("D:\\IDEA workspace\\email-parser\\src\\test\\resources\\yamail-yamail\\wc_test.eml"))
-    val message: MimeMessage = MimeMessage(mailSession, source)
-
-    //    val sc: Scanner = Scanner(FileInputStream(File("src\\main\\resources\\1_1_send_2_q0.eml")))
-    //    var sb = StringBuffer()
-    //    while (sc.hasNextLine()) {
-    //        sb.append(sc.nextLine()+ "\n")
-    //    }
-    //    val text = sb.toString()
-    //    println(text)
-    //    val source: InputStream = ByteArrayInputStream(text.toByteArray(Charset.defaultCharset()))
-    //    val message: MimeMessage = MimeMessage(mailSession, source)
-
-    println("Date : " + message.sentDate)
-    for (a in message.from) {
-        val addr = a as InternetAddress
-        println("From : ${addr.address}")
-    }
-    for (t in message.getRecipients(Message.RecipientType.TO)) {
-        val addr = t as InternetAddress
-        println("To : ${addr.address}")
-    }
-    println("Subject : " + message.subject)
-    println("--------------")
-    val content = message.content
-    if (content is MimeMultipart) {
-        println(content.getBodyPart(0).content)
-    } else {
-        println(content)
-    }
-
-
-    //    val headers =  message.allHeaders
-    //    while (headers.hasMoreElements()) {
-    //        val h: Header = headers.nextElement() as Header
-    //        println("${h.name}: ${h.value}")
-    //    }
 }
