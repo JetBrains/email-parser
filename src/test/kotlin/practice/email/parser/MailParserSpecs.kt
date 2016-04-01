3e20b36fe2c3bbe0a3aeed91eee17c643fc34fc6
Zhuk Pavel <ppzhuk@users.noreply.github.com>
2016-04-02 01:54:57 +0300

    modify tests (task 4-5)

--- src/test/kotlin/practice/email/parser/MailParserSpecs.kt
+++ src/test/kotlin/practice/email/parser/MailParserSpecs.kt
@@ -13,7 +13,7 @@ class MailParserSpecs : Spek() {
             on("simple email") {
                 var expectedEmail: Email = TestEmails.yam_tb_eng_rus
                 var actualEmail: Email = parseEml(
-                        File("""D:\IDEA workspace\email-parser\src\test\resources\yam_tb_eng_rus.eml""")
+                        File("""D:\IDEA workspace\email-parser\src\test\resources\emailTests\simple.eml""")
                 )
 
                 it("should parse DATE correct") {
@@ -38,40 +38,11 @@ class MailParserSpecs : Spek() {
                     assertNull(actualEmail.content.signature)
                 }
             }
-
-            on("email with 5 quotes") {
-                var expectedEmail = TestEmails.gmail_tb_email_parser_task2_5_quotes
-                var actualEmail = parseEml(
-                        File("""D:\IDEA workspace\email-parser\src\test\resources\gmail_tb_email_parser_task2_5_quotes.eml""")
-                )
-
-                it("should parse DATE correct") {
-                    assertEquals(expectedEmail.date, actualEmail.date)
-                }
-                it("should parse FROM addresses correct") {
-                    assertArrayEquals(expectedEmail.from.toArray(), actualEmail.from.toArray())
-                }
-                it("should parse TO addresses correct") {
-                    assertArrayEquals(expectedEmail.to.toArray(), actualEmail.to.toArray())
-                }
-                it("should parse SUBJECT correct") {
-                    assertEquals(expectedEmail.subject, actualEmail.subject)
-                }
-                it("should parse CONTENT BODY correct") {
-                    assertEquals(expectedEmail.content.body, actualEmail.content.body)
-                }
-                it("shouldn't have QUOTES") {
-                    assertNull(actualEmail.content.quote)
-                }
-                it("shouldn't have SIGNATURE") {
-                    assertNull(actualEmail.content.signature)
-                }
-            }
-
+            
             on("X - email with KOI8-R content-type charset") {
                 var expectedEmail = TestEmails.wc_test
                 var actualEmail = parseEml(
-                        File("""D:\IDEA workspace\email-parser\src\test\resources\wc_test.eml""")
+                        File("""D:\IDEA workspace\email-parser\src\test\resources\emailTests\simple_koi8.eml""")
                 )
 
                 it("should parse DATE correct") {
@@ -102,7 +73,7 @@ class MailParserSpecs : Spek() {
             on("email with base64 Content-Transfer-Encoding") {
                 var expectedEmail = TestEmails.gmailMariya
                 var actualEmail = parseEml(
-                        File("""D:\IDEA workspace\email-parser\src\test\resources\gmailMariya.eml""")
+                        File("""D:\IDEA workspace\email-parser\src\test\resources\emailTests\simple_base64_encoding.eml""")
                 )
 
                 it("should parse DATE correct") {
@@ -131,7 +102,7 @@ class MailParserSpecs : Spek() {
             on("X - another email with KOI8-R content-type charset") {
                 var expectedEmail = TestEmails.e6_2_receive_q2
                 var actualEmail = parseEml(
-                        File("""D:\IDEA workspace\email-parser\src\test\resources\6_2_receive_q2.eml""")
+                        File("""D:\IDEA workspace\email-parser\src\test\resources\emailTests\simple_koi8_DECODE_FAILED.eml""")
                 )
 
                 it("should parse DATE correct") {
@@ -161,7 +132,7 @@ class MailParserSpecs : Spek() {
             on("FWD multipart/alternative email") {
                 var expectedEmail = TestEmails.fwd_gmail_tb
                 var actualEmail = parseEml(
-                        File("""D:\IDEA workspace\email-parser\src\test\resources\Fwd_gmail_tb.eml""")
+                        File("""D:\IDEA workspace\email-parser\src\test\resources\emailTests\multipart_alt.eml""")
                 )
 
                 it("should parse DATE correct") {
@@ -190,7 +161,7 @@ class MailParserSpecs : Spek() {
             on("FWD multipart/alternative KOI8-R email") {
                 var expectedEmail = TestEmails.fwd_yam_tb
                 var actualEmail = parseEml(
-                        File("""D:\IDEA workspace\email-parser\src\test\resources\Fwd_yam_tb.eml""")
+                        File("""D:\IDEA workspace\email-parser\src\test\resources\emailTests\multipart_alt_koi8.eml""")
                 )
 
                 it("should parse DATE correct") {
@@ -219,7 +190,7 @@ class MailParserSpecs : Spek() {
             on("Outlook multipart/alternative US-ASCII email") {
                 var expectedEmail = TestEmails.from_outlook_eng
                 var actualEmail = parseEml(
-                        File("""D:\IDEA workspace\email-parser\src\test\resources\from_outlook_eng.eml""")
+                        File("""D:\IDEA workspace\email-parser\src\test\resources\emailTests\multipert_alt_ascii_CONTENT_TYPE_SEPARATE_LINES_and_CHARSET_QUOTES.eml""")
                 )
 
                 it("should parse DATE correct") {
@@ -246,9 +217,9 @@ class MailParserSpecs : Spek() {
             }
 
             on("Outlook multipart/alternative KOI8-R email") {
-                var expectedEmail = TestEmails.from_outlook_eng
+                var expectedEmail = TestEmails.from_outlook_ru
                 var actualEmail = parseEml(
-                        File("""D:\IDEA workspace\email-parser\src\test\resources\from_outlook_eng.eml""")
+                        File("""D:\IDEA workspace\email-parser\src\test\resources\emailTests\multipart_alt_koi8_CONTENT_TYPE_SEPARATE_LINES_and_CHARSET_QUOTES.eml""")
                 )
 
                 it("should parse DATE correct") {
