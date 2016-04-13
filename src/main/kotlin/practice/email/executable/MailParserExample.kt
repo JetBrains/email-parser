fdb470cbe9718df70f081d7b5bbc38703a8a6010
Zhuk Pavel <ppzhuk@users.noreply.github.com>
2016-04-13 12:59:09 +0300

    support tree-based presentation

--- src/main/kotlin/practice/email/executable/MailParserExample.kt
+++ src/main/kotlin/practice/email/executable/MailParserExample.kt
@@ -2,6 +2,7 @@ package practice.email.executable
 
 import practice.email.parser.Content
 import practice.email.parser.ContentParseMode
+import practice.email.parser.Email
 import practice.email.parser.EmailContentParser
 import practice.email.parser.EmailParser
 import java.io.File
@@ -13,41 +14,9 @@ fun main(args: Array<String>) {
     if (args.size > 0) {
         val file = File(args[0])
         if (file.exists()) {
-            val parser = EmailParser(file)
-            var email1 = parser.parse()
-            println("********** Just an email **********")
-            println(email1)
-
-            parser.prepare(mode = ContentParseMode.MODE_ONE)
-            var email2 = parser.parse()
-            println("********** Content's components (mode ONE) **********")
-            println("*** body ***")
-            println(email2.content.body)
-            println("*** quote ***")
-            println(email2.content.quote)
-            println("*** signature ***")
-            println(email2.content.signature)
-            println()
-
-            parser.prepare(mode = ContentParseMode.MODE_DEEP)
-            var email3 = parser.parse()
-            println("********** Content's components (mode DEEP) **********")
-            
-            var currentContent: Content? = email3.content
-            var i = 0
-            do {
-                println("*** body$i ***")
-                println(currentContent?.body)
-                println("*** quote$i ***")
-                println(currentContent?.quote)
-                println("*** signature$i ***")
-                println(currentContent?.signature)
-                println()
-                
-                currentContent = currentContent?.quote
-                ++i
-            } while (currentContent != null)
-
+            val parser = EmailParser(file, ContentParseMode.MODE_DEEP)
+            var email = parser.parse()
+            println(email.getTreeRepresentation())
         } else {
             println(incorrectFileMsg)
         }
--- src/main/kotlin/practice/email/parser/Email.kt
+++ src/main/kotlin/practice/email/parser/Email.kt
@@ -11,9 +11,25 @@ data class Email(val date: Date,
                  val subject: String,
                  var content: Content) {
 
+    private val forkSeparator = "+---"
+    private val leafSeparator = "|___"
+    
     override fun toString(): String {
         val builder = StringBuilder()
+        getHeaders(builder)
+        builder.append("-----------------------------------\n")
+        builder.append(content)
+        return builder.toString()
+    }
+    
+    fun getTreeRepresentation(): String {
+        val builder = StringBuilder()
+        getHeaders(builder)
+        getContent(content, StringBuilder(""), builder)
+        return builder.toString()
+    }
 
+    private fun getHeaders(builder: StringBuilder) {
         builder.append("Date: $date\n")
         for (f in from) {
             builder.append("From: $f\n")
@@ -22,8 +38,48 @@ data class Email(val date: Date,
             builder.append("To: $t\n")
         }
         builder.append("Subject: $subject\n")
-        builder.append("-----------------------------------\n")
-        builder.append(content)
-        return builder.toString()
+    }
+
+    private fun getContent(content: Content, paddingBuilder: StringBuilder, resultBuilder: StringBuilder) {
+        val bodyPaddingBuilder = StringBuilder(paddingBuilder)
+        var noSignature = content.signature == null
+        if (noSignature) {
+            bodyPaddingBuilder.append(" ")
+        } else {
+            bodyPaddingBuilder.append("|")
+        }
+
+        resultBuilder.append(paddingBuilder.toString())
+        resultBuilder.append(
+                if (noSignature)
+                    leafSeparator
+                else
+                    forkSeparator
+        )
+        resultBuilder.append("\n")
+
+        val bodyPadding = bodyPaddingBuilder.toString()
+        val body = content.body.split("\n").toTypedArray()
+        for (line in body) {
+            resultBuilder.append(bodyPadding)
+            resultBuilder.append("$line\n")
+        }
+
+        if (content.quote != null) {
+            getContent(content.quote, bodyPaddingBuilder.append("   "), resultBuilder)
+        }
+
+        if (content.signature != null) {
+
+            resultBuilder.append(paddingBuilder.toString())
+            resultBuilder.append("$leafSeparator\n")
+
+            val signPadding = paddingBuilder.append(" ").toString()
+            val sign = content.signature.split("\n").toTypedArray()
+            for (line in sign) {
+                resultBuilder.append(signPadding)
+                resultBuilder.append("$line\n")
+            }
+        }
     }
 }
